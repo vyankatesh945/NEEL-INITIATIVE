1cbebb46476c2fac6e02c135e556a7a53be1ed2d
Vikrant Yadav <vikrantmani45@gmail.com>
2024-10-14 15:35:56 +0530

    zoom-in effect on main home page heading

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
   return (
     <section className="home" id="home">
       <div className="content">
-        <h1 data-aos= "fade-down">Welcome to NEEL-INITIATIVE: Pioneering the Future with AI
+        <h1 data-aos= "zoom-in">Welcome to NEEL-INITIATIVE: Pioneering the Future with AI
         </h1>
         <p data-aos="fade-right">
         At NEEL-INITIATIVE, we are at the forefront of revolutionizing industries through cutting-edge AI solutions. <br></br>From empowering Indian farmers with precision technology to advancing medical diagnostics, self-driving vehicles, <br></br>and space exploration, we are committed to making tomorrow’s innovations accessible today. Join us as we create smarter, <br></br>more efficient systems for a better, more connected world.
