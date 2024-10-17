168a881ee06165759831f9994a7ee0b4b2911613
Vikrant Yadav <vikrantmani45@gmail.com>
2024-10-17 10:43:47 +0530

    mobile responsiveness changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMobile(!isMobile);
+  };
+
   return (
     <nav className="navbar">
       <div className="logo">
         <img src="./images/logo.jpg" alt="Neel Initiatives" />
       </div>
-      <ul className="nav-links">
+
+      <div className={`nav-links ${isMobile ? "mobile-menu" : ""}`}>
         <li><a href="#home">Home</a></li>
         <li><a href="#achievements">Achievements</a></li>
         <li><a href="#work">Work</a></li>
         <li><a href="#projects">Projects</a></li>
         {/* <li><a href="#team">Meet Our Team</a></li> */}
-      </ul>
+      </div>
+
+      <div className="menu-icon" onClick={toggleMenu}>
+        <i className={isMobile ? "fas fa-times" : "fas fa-bars"}></i>
+      </div>
+
       <div className="auth-buttons">
         <button className="sign-in">Sign In</button>
         <button className="get-started">Get Started</button>
